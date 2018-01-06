8adabe0119cc87d7b92c496613ecfcb70862fad6
brandicph <alexander@pbrandi.dk>
2018-01-06 12:40:08 +0100

    Added qp class

--- python/tools/qp.py
+++ python/tools/qp.py
@@ -23,7 +23,7 @@ from pandas.plotting import andrews_curves
 https://www.laroccasolutions.com/164-rsrq-to-sinr/
 """
 
-script_path = os.path.dirname(os.path.abspath( __file__ ))
+SCRIPT_PATH = os.path.dirname(os.path.abspath( __file__ ))
 # Ostenfeldt Ping
 #CSV_IN_FILEPATH = os.path.join(script_path, '../../data/2018-01-03-00-44-12-0000-5310-7746-0004-S_ping.csv')
 # DTU 1GB download
@@ -31,8 +31,8 @@ script_path = os.path.dirname(os.path.abspath( __file__ ))
 # Lavensby 1GB download
 #CSV_IN_FILEPATH = os.path.join(script_path, '../../data/2017-12-06-12-34-57-0000-5310-7746-0004-S_ping.csv')
 # S-TOG B (DTU) Ping
-CSV_IN_FILEPATH = os.path.join(script_path, '../../data/2018-01-06-08-55-34-0000-5310-7746-0004-S_ping_B.csv')
-CSV_OUT_FILEPATH = os.path.join(script_path, '../../data/measurement_data.csv')
+CSV_IN_FILEPATH = os.path.join(SCRIPT_PATH, '../../data/2018-01-06-08-55-34-0000-5310-7746-0004-S_ping_B.csv')
+CSV_OUT_FILEPATH = os.path.join(SCRIPT_PATH, '../../data/measurement_data.csv')
 
 class QualiPoc(object):
 
@@ -147,94 +147,3 @@ def rolling_mean(data, axis=0):
     return pd.rolling_mean(data, 4, axis=axis).mean(axis=axis)
 
 
-qp = QualiPoc(CSV_IN_FILEPATH)
-
-print(len(qp.df))
-df = qp.df.dropna(subset=['Intermediate KPI', 'SINR Rx[0]', 'SINR Rx[1]'])
-df = df[df['Intermediate KPI'] > 0]
-print(len(df))
-
-
-mpl.style.use('seaborn')
-
-
-fig, ax1 = plt.subplots()
-s10 = df['SINR Rx[0]']
-s11 = df['SINR Rx[1]']
-ax1.plot(s10, color='#4C72B0')
-ax1.plot(s11, color='#55A868')
-ax1.set_xlabel('measurements')
-ax1.set_ylabel('SINR Rx[0] / Rx[1] (dB)')
-ax1.legend(['SINR Rx[0]', 'SINR Rx[1]'], loc='upper right', bbox_to_anchor=(1.0, 1.0), ncol=2)
-ax1.tick_params('y')
-#ax1.set_yscale('log', basey=10)
-
-ax2 = ax1.twinx()
-s20 = df['RSRP Rx[0]']
-s21 = df['RSRP Rx[1]']
-ax2.plot(s20, color='#C44E52')
-ax2.plot(s21, color='#8172B2')
-ax2.set_ylabel('RSRP Rx[0] / Rx[1] (dBm)')
-ax2.legend(['RSRP Rx[0]', 'RSRP Rx[1]'], loc='upper right', bbox_to_anchor=(1.0, 0.95), ncol=2)
-ax2.tick_params('y')
-#ax2.set_yscale('log', basey=2)
-ax2.grid(False)
-fig.tight_layout()
-
-plt.show(block=False)
-
-# Perform rolling mean
-df_sinr_rsrp_rolling = df[['SINR Rx[0]', 'RSRP Rx[0]', 'SINR Rx[1]', 'RSRP Rx[1]']].rolling(200, win_type='triang').sum()
-
-# SINR0 / RSRP0
-fig, ax1 = plt.subplots()
-s10 = df_sinr_rsrp_rolling['SINR Rx[0]']
-ax1.plot(s10, color='#4C72B0')
-ax1.set_xlabel('Measurements')
-ax1.set_ylabel('SINR Rx[0] (dB)')
-ax1.legend(['SINR Rx[0]'], loc='upper right', bbox_to_anchor=(1.0, 1.0), ncol=2)
-ax1.tick_params('y')
-#ax1.set_yscale('log', basey=10)
-
-ax2 = ax1.twinx()
-s20 = df_sinr_rsrp_rolling['RSRP Rx[0]']
-ax2.plot(s20, color='#55A868')
-ax2.set_ylabel('RSRP Rx[0] (dBm)')
-ax2.legend(['RSRP Rx[0]'], loc='upper right', bbox_to_anchor=(1.0, 0.95), ncol=2)
-ax2.tick_params('y')
-#ax2.set_yscale('log', basey=2)
-ax2.grid(False)
-fig.tight_layout()
-
-plt.show(block=False)
-
-# SINR1 / RSRP1
-fig, ax1 = plt.subplots()
-s10 = df_sinr_rsrp_rolling['SINR Rx[1]']
-ax1.plot(s10, color='#C44E52')
-ax1.set_xlabel('Measurements')
-ax1.set_ylabel('SINR Rx[1] (dB)')
-ax1.legend(['SINR Rx[1]'], loc='upper right', bbox_to_anchor=(1.0, 1.0), ncol=2)
-ax1.tick_params('y')
-#ax1.set_yscale('log', basey=10)
-
-ax2 = ax1.twinx()
-s20 = df_sinr_rsrp_rolling['RSRP Rx[1]']
-ax2.plot(s20, color='#8172B2')
-ax2.set_ylabel('RSRP Rx[1] (dBm)')
-ax2.legend(['RSRP Rx[1]'], loc='upper right', bbox_to_anchor=(1.0, 0.95), ncol=2)
-ax2.tick_params('y')
-#ax2.set_yscale('log', basey=2)
-ax2.grid(False)
-fig.tight_layout()
-
-
-#plt.title('SINR vs. RSRP\nwithout preprocessing')
-#['#4C72B0', '#55A868', '#C44E52', '#8172B2', '#CCB974', '#64B5CD']
-#print(mpl.rcParams['axes.prop_cycle'])
-
-qp.logger.info('Showing plot...')
-
-plt.show()
-
-qp.logger.info('Exit...')
