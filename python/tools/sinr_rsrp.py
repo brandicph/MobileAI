aae5760c931ae915f635cb045ca3bce6e7a52951
brandicph <alexander@pbrandi.dk>
2018-01-18 20:44:41 +0100

    Added new 308 data

--- python/tools/sinr_rsrp.py
+++ python/tools/sinr_rsrp.py
@@ -37,7 +37,11 @@ mpl.rcParams.update(pgf_with_custom_preamble)
 SCRIPT_PATH = os.path.dirname(os.path.abspath( __file__ ))
 #CSV_IN_FILEPATH = os.path.join(SCRIPT_PATH, '../../data/2018-01-06-08-55-34-0000-5310-7746-0004-S_ping_B.csv')
 # Airport Ping
-CSV_IN_FILEPATH = os.path.join(SCRIPT_PATH, '../../data/2018-01-06-16-27-25-0000-5310-7746-0004-S_airport_ping2.csv')
+#CSV_IN_FILEPATH = os.path.join(SCRIPT_PATH, '../../data/2018-01-06-16-27-25-0000-5310-7746-0004-S_airport_ping2.csv')
+# DTU 1GB download
+CSV_IN_FILEPATH = os.path.join(SCRIPT_PATH, '../../data/2017-11-27-14-07-44-0000-5310-7746-0004-S_ping2.csv')
+# Lavensby 1GB download
+#CSV_IN_FILEPATH = os.path.join(SCRIPT_PATH, '../../data/2017-12-06-12-34-57-0000-5310-7746-0004-S_ping2.csv')
 # Instatiate data reader
 qp = QualiPoc(CSV_IN_FILEPATH)
 
@@ -56,7 +60,7 @@ s10 = df['SINR Rx[0]']
 s11 = df['SINR Rx[1]']
 ax1.plot(s10, color='#4C72B0')
 ax1.plot(s11, color='#55A868')
-ax1.set_xlabel('measurements')
+ax1.set_xlabel('Measurements')
 ax1.set_ylabel('SINR Rx[0] / Rx[1] (dB)')
 ax1.legend(['SINR Rx[0]', 'SINR Rx[1]'], loc='upper right', bbox_to_anchor=(1.0, 1.0), ncol=2)
 ax1.tick_params('y')
@@ -126,28 +130,59 @@ fig.tight_layout()
 fig, ax1 = plt.subplots()
 s10 = df['SINR Rx[0]']
 s11 = df['SINR Rx[1]']
-ax1.plot(s10, color='#C44E52')
-ax1.plot(s11, color='grey')
-ax1.set_xlabel('Time')
+ax1.plot(s10, color='#333333')
+ax1.plot(s11, color='#C44E52')
+ax1.set_xlabel('Measurement')
 ax1.set_ylabel('SINR (dB)')
 ax1.legend(['SINR $Rx_0$', 'SINR $Rx_1$'], loc='upper right')
 ax1.tick_params('y')
 #ax1.set_yscale('log', basey=10)
 fig.tight_layout()
 plt.show(block=False)
+fig.savefig("lineplot_sinr_raw.pdf")
 
 fig, ax1 = plt.subplots()
 s20 = df['RSRP Rx[0]']
 s21 = df['RSRP Rx[1]']
-ax1.plot(s20, color='#C44E52')
-ax1.plot(s21, color='grey')
+ax1.plot(s20, color='#333333')
+ax1.plot(s21, color='#C44E52')
 ax1.set_ylabel('RSRP (dBm)')
-ax1.set_xlabel('Time')
+ax1.set_xlabel('Measurement')
 ax1.legend(['RSRP $Rx_0$', 'RSRP $Rx_1$'], loc='upper right')
 ax1.tick_params('y')
 #ax2.set_yscale('log', basey=2)
 fig.tight_layout()
 plt.show(block=False)
+fig.savefig("lineplot_rsrp_raw.pdf")
+
+
+
+def autolabel(rects, ax):
+    """
+    Attach a text label above each bar displaying its height
+    """
+    for rect in rects:
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
+                '%d' % int(height),
+                ha='center', va='bottom')
+
+
+fig, ax1 = plt.subplots()
+s10 = df#[df['Intermediate KPI'] > 0]
+s10 = s10['Intermediate KPI']
+ind = np.arange(len(s10))
+rects = ax1.plot(ind, s10, color='#C44E52')
+ax1.set_xlabel('Measurement')
+ax1.set_ylabel('Intermediate KPI (kbps)')
+ax1.legend(['Intermediate KPI (RTT)'], loc='upper right')
+ax1.tick_params('y')
+#ax1.set_yscale('log', basey=10)
+#autolabel(rects,ax1)
+#ax1.set_ylim((0, 200))
+fig.tight_layout()
+plt.show(block=False)
+fig.savefig("lineplot_intermediate_kpi_raw.pdf")
 
 
 
