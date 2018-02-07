9b065685287ff7920b0ece7a1d2661448345c96f
brandicph <alexander@pbrandi.dk>
2018-02-07 06:16:12 +0100

    Added styles

--- app/android/app/src/main/java/dk/dtu/mobileai/MainActivity.java
+++ app/android/app/src/main/java/dk/dtu/mobileai/MainActivity.java
@@ -20,6 +20,10 @@ import android.telephony.CellInfoLte;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -111,6 +115,8 @@ public class MainActivity extends AppCompatActivity {
 
         //setup content stuff
         this.setContentView(R.layout.activity_main);
+
+        //setStatusBarColor(findViewById(R.id.statusBarBackground),getResources().getColor(android.R.color.white));
 
         signalStrengthTextView = (TextView) findViewById(R.id.signalStrengthTextView);
         signalStrengthTextView2 = (TextView) findViewById(R.id.signalStrengthTextView2);
@@ -256,13 +262,14 @@ public class MainActivity extends AppCompatActivity {
         mSeriesRSRP1.setDrawDataPoints(false);
         mSeriesRSRP1.setDrawBackground(true);
         mSeriesRSRP1.setTitle("RSRP 0");
+        mSeriesRSRP1.setColor(Color.parseColor("#555555"));
         graph.addSeries(mSeriesRSRP1);
 
         mSeriesRSRP2 = new LineGraphSeries<>();
         mSeriesRSRP2.setDrawDataPoints(false);
         mSeriesRSRP2.setDrawBackground(true);
         mSeriesRSRP2.setTitle("RSRP 1");
-        mSeriesRSRP2.setColor(Color.GREEN);
+        mSeriesRSRP2.setColor(Color.parseColor("#B50404"));
         graph.addSeries(mSeriesRSRP2);
 
         graph.getGridLabelRenderer().setHighlightZeroLines(false);
@@ -276,6 +283,36 @@ public class MainActivity extends AppCompatActivity {
         graph.getLegendRenderer().setVisible(true);
         graph.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
         graph.getLegendRenderer().setBackgroundColor(Color.TRANSPARENT);
+    }
+
+    public void setStatusBarColor(View statusBar, int color){
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        //status bar height
+        int actionBarHeight = getActionBarHeight();
+        int statusBarHeight = getStatusBarHeight();
+        //action bar height
+        statusBar.getLayoutParams().height = actionBarHeight + statusBarHeight;
+        statusBar.setBackgroundColor(color);
+    }
+
+    public int getActionBarHeight() {
+        int actionBarHeight = 0;
+        TypedValue tv = new TypedValue();
+        if (getTheme().resolveAttribute(android.R.attr.actionBarSize, tv, true))
+        {
+            actionBarHeight = TypedValue.complexToDimensionPixelSize(tv.data,getResources().getDisplayMetrics());
+        }
+        return actionBarHeight;
+    }
+
+    public int getStatusBarHeight() {
+        int result = 0;
+        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
     }
 
     public void onResume() {
